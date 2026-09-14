12fe471965ff92444e51a8d39a6e22c2b6fa7eaf
kkarmp <katerinauop@gmail.com>
2026-09-14 14:15:48 +0300

    Add UserRepository for user data management

---
+++ repositories/user_repository.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from models.user import User
+
+class UserRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_by_username(self, username: str):
+        return self.db.query(User).filter(User.username == username).first()
+
+    def get_by_email(self, email: str):
+        return self.db.query(User).filter(User.email == email).first()
+
+    def create(self, user: User):
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
