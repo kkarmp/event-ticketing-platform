fd26c5f1f8daced049a9983ba65597a1b8ec4f96
kkarmp <katerinauop@gmail.com>
2026-09-14 14:16:03 +0300

    Add EventRepository with CRUD methods
    Implement EventRepository for managing Event records

---
+++ repositories/event_repository.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from models.event import Event
+
+class EventRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all(self):
+        return self.db.query(Event).all()
+
+    def get_by_id(self, event_id: int):
+        return self.db.query(Event).filter(Event.id == event_id).first()
+
+    def create(self, event: Event):
+        self.db.add(event)
+        self.db.commit()
+        self.db.refresh(event)
+        return event
+
+    def update(self):
+        self.db.commit()
